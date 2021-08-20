c139dc952b6571ee06498cca081686c1d6b7a764
Dhanushrao02 <84696628+Dhanushrao02@users.noreply.github.com>
2021-08-20 22:10:48 +0530

    Update Program-6.c

--- Program-6.c
+++ Program-6.c
@@ -3,18 +3,20 @@
 	input: 3 6
 	output: 9*/
 	#include<stdio.h>
-	int sum(int a,int b)
-	{ int c;
-	   c=a+b;
-     return c; //Complete the missing code
+	int increment(int var)
+	{
+	    var=var+1;
+	    return var;
 	}
 	int main()
 	{
-	    int var1,var2,var3;
-	    printf("Enter two numbers:");
-	    scanf("%d%d",&var1,&var2);
-	    var3=sum(var1,var2);
-	    printf("%d",var3);
-	    return 0;
+	 //Declare the variables
+   int num1,num2;
+	   printf("Enter num1:");
+	   scanf("%d",&num1);
+	   num2=increment(num1);
+	   printf("num2 value is:%d",num2);
+     //enter the missing data
+	   return 0;
 	}
 	
